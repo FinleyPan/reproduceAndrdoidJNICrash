bc8ec1daa08ba4ee9ade958716ee9ba32edebc49
panfeng <panfeng@xiaohongshu.com>
2022-12-12 18:55:41 +0800

    Cmake native logs

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,14 @@
 #include <jni.h>
 #include <string>
 
+#include <android/log.h>
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_reproducecrash_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
+
+    __android_log_print(ANDROID_LOG_INFO, "MyLogger", "Logging in JNI");
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
